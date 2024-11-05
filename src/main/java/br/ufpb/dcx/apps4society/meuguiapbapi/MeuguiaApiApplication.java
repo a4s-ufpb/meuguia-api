1d04bfa0e6c9f75ed23bf82f41c76278bcaf39bd
Nycolas Kevin <nycolas.kevin2@gmail.com>
2024-11-05 18:45:35 -0300

    ci: rename dev workflow, add deploy workflow, changes in docker-compose

--- src/main/java/br/ufpb/dcx/apps4society/meuguiapbapi/MeuguiaApiApplication.java
+++ src/main/java/br/ufpb/dcx/apps4society/meuguiapbapi/MeuguiaApiApplication.java
@@ -1,13 +1,25 @@
 package br.ufpb.dcx.apps4society.meuguiapbapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class MeuguiaApiApplication {
+	@Value("${app.version}")
+	private String appVersion;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuguiaApiApplication.class, args);
 	}
 
+	@GetMapping("/")
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public String home() {
+		return "Bem vindo ao MeuGuiaPB API. Versão: " + appVersion;
+	}
 }
